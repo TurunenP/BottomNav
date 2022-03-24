fb2692716a5f7748f8b8736c11a78f725d303a76
Purity Turunen <k7jepu00@students.oamk.fi>
2022-03-24 16:48:09 +0200

    First commit

--- app/src/main/java/com/example/bottomnav/HomeFragment.kt
+++ app/src/main/java/com/example/bottomnav/HomeFragment.kt
@@ -16,4 +16,8 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+        companion object {
+
+            fun newInstance() = HomeFragment()
+    }
 }
